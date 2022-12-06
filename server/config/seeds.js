399d751ce69f3ceae55e45a0d3270489e00057d7
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-12-05 22:17:27 -0500

    Update seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -14,35 +14,35 @@ db.once('open', async () => {
         commentAuthor: 'User100'
     })
 
-    // await Comment.create({
-    //     commentText: 'Dang Cardinals ruined my bet.',
-    //     createdAt: '',
-    //     commentAuthor: 'User102'
-    // })
+    const comment3 = await Comment.create({
+         commentText: 'Dang Cardinals ruined my bet.',
+         createdAt: '',
+         commentAuthor: 'User102'
+    })
 
-    // await Comment.create({
-    //     commentText: 'I always knew the Giants were frauds. They wont win another game this year.',
-    //     createdAt: '',
-    //     commentAuthor: 'User103'
-    // })
+    const comment4 = await Comment.create({
+         commentText: 'I always knew the Giants were frauds. They wont win another game this year.',
+         createdAt: '',
+         commentAuthor: 'User103'
+     })
 
-    // await Comment.create({
-    //     commentText: 'NFC is so wide open. I wonder whos going to win the conference.',
-    //     createdAt: '',
-    //     commentAuthor: 'User104'
-    // })
+    const comment5 = await Comment.create({
+         commentText: 'NFC is so wide open. I wonder whos going to win the conference.',
+         createdAt: '',
+         commentAuthor: 'User104'
+     })
 
-    // await Comment.create({
-    //     commentText: 'I wonder how truly healthy Josh Allen is. Bills wont win anything without him.',
-    //     createdAt: '',
-    //     commentAuthor: 'User105'
-    // })
+     const comment6 = await Comment.create({
+         commentText: 'I wonder how truly healthy Josh Allen is. Bills wont win anything without him.',
+         createdAt: '',
+         commentAuthor: 'User105'
+     })
 
-    // await Comment.create({
-    //     commentText: 'Man Pat Mahomes is a baaaaaad man.',
-    //     createdAt: '',
-    //     commentAuthor: 'User106'
-    // })
+    const comment7 = await Comment.create({
+         commentText: 'Man Pat Mahomes is a baaaaaad man.',
+         createdAt: '',
+         commentAuthor: 'User106'
+     })
 
     // await Comment.create({
     //     commentText: '49ers defense is lethal!!! Nick Bosa DPOY.',
@@ -75,41 +75,42 @@ db.once('open', async () => {
         comments: [comment1, comment2]
     });
 
-    // await User.create({
-    //     username: 'user101',
-    //     email: 'user101@gmail.com',
-    //     password: 'password2',
-    // });
+     await User.create({
+         username: 'user102',
+         email: 'user101@gmail.com',
+         password: 'password2',
+         comments: [comment3]
+     });
 
-    // await User.create({
-    //     username: 'user102',
-    //     email: 'user102@gmail.com',
-    //     password: 'password3',
-    // })
+     await User.create({
+         username: 'user103',
+         email: 'user102@gmail.com',
+         password: 'password3',
+         comments: [comment4]
+     })
 
-    // await User.create({
-    //     username: 'user103',
-    //     email: 'user103@gmail.com',
-    //     password: 'password4',
-    // })
+    
 
-    // await User.create({
-    //     username: 'user104',
-    //     email: 'user104@gmail.com',
-    //     password: 'password5',
-    // })
+     await User.create({
+         username: 'user104',
+         email: 'user104@gmail.com',
+         password: 'password5',
+         comments: [comment5]
+        })
 
-    // await User.create({
-    //     username: 'user105',
-    //     email: 'user105@gmail.com',
-    //     password: 'password6',
-    // })
+     await User.create({
+         username: 'user105',
+         email: 'user105@gmail.com',
+         password: 'password6',
+         comments: [comment6]
+     })
 
-    // await User.create({
-    //     username: 'user106',
-    //     email: 'user106@gmail.com',
-    //     password: 'password6',
-    // })
+     await User.create({
+         username: 'user106',
+         email: 'user106@gmail.com',
+         password: 'password6',
+            comments: [comment7]
+     })
 
     // await User.create({
     //     username: 'user107',
